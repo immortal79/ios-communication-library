b17fb109cf3513ceb8c3797d34609030d0defd19
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2020-04-14 13:52:31 +0200

    Fix for #10
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- NCCommunication/NCCommunicationCommon.swift
+++ NCCommunication/NCCommunicationCommon.swift
@@ -56,10 +56,7 @@ import Alamofire
     @objc let sessionIdentifierBackground: String = "com.nextcloud.session.background"
     @objc let sessionIdentifierBackgroundwifi: String = "com.nextcloud.session.backgroundwifi"
     @objc let sessionIdentifierExtension: String = "com.nextcloud.session.extension"
-    
-    // Constant
-    let k_encodeCharacterSet = " #;?@&=$+{}<>,!'*"
-    
+        
     //MARK: - Setup
     
     @objc public func setup(user: String, userId: String, password: String, url: String, userAgent: String?, capabilitiesGroup: String?, nextcloudVersion: Int, delegate: NCCommunicationCommonDelegate?) {
@@ -149,8 +146,7 @@ import Alamofire
         
     func encodeStringToUrl(_ string: String) -> URLConvertible? {
         
-        let allowedCharacterSet = (CharacterSet(charactersIn: k_encodeCharacterSet).inverted)
-        if let escapedString = string.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet) {            
+        if let escapedString = string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
             var url: URLConvertible
             do {
                 try url = escapedString.asURL()
@@ -163,9 +159,7 @@ import Alamofire
     }
     
     func encodeString(_ string: String) -> String? {
-        
-        let allowedCharacterSet = (CharacterSet(charactersIn: k_encodeCharacterSet).inverted)
-        return string.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)
+        return string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
     }
         
     func StringToUrl(_ string: String) -> URLConvertible? {
